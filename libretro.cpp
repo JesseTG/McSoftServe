3a752df34e774d462758c6f7e11d18fde6428e3a
Jesse Talavera <jesse@jesse.tg>
2024-03-26 18:58:16 -0400

    Add all missing callbacks

--- libretro.cpp
+++ libretro.cpp
@@ -11,6 +11,10 @@ struct MachineState
 namespace
 {
     retro_video_refresh_t _video_refresh = nullptr;
+    retro_audio_sample_t _audio_sample = nullptr;
+    retro_audio_sample_batch_t _audio_sample_batch = nullptr;
+    retro_input_poll_t _input_poll = nullptr;
+    retro_input_state_t _input_state = nullptr;
     retro_environment_t _environment = nullptr;
     retro_log_printf_t _log = nullptr;
 
@@ -29,19 +33,38 @@ RETRO_API void retro_set_video_refresh(retro_video_refresh_t refresh)
     _video_refresh = refresh;
 }
 
-RETRO_API void retro_set_audio_sample(retro_audio_sample_t);
-RETRO_API void retro_set_audio_sample_batch(retro_audio_sample_batch_t);
-RETRO_API void retro_set_input_poll(retro_input_poll_t);
-RETRO_API void retro_set_input_state(retro_input_state_t);
+RETRO_API void retro_set_audio_sample(retro_audio_sample_t audio_sample)
+{
+    _audio_sample = audio_sample;
+}
+
+RETRO_API void retro_set_audio_sample_batch(retro_audio_sample_batch_t audio_sample_batch)
+{
+    _audio_sample_batch = audio_sample_batch;
+}
+
+RETRO_API void retro_set_input_poll(retro_input_poll_t input_poll)
+{
+    _input_poll = input_poll;
+}
+
+RETRO_API void retro_set_input_state(retro_input_state_t input_state)
+{
+    _input_state = input_state;
+}
 
 RETRO_API void retro_set_environment(retro_environment_t env)
 {
     _environment = env;
     retro_log_callback log = { .log = nullptr };
     bool yes = true;
+    retro_pixel_format format = RETRO_PIXEL_FORMAT_XRGB8888;
     _environment(RETRO_ENVIRONMENT_SET_SUPPORT_NO_GAME, &yes);
     _environment(RETRO_ENVIRONMENT_GET_LOG_INTERFACE, &log);
+    _environment(RETRO_ENVIRONMENT_SET_PIXEL_FORMAT, &format);
     _log = log.log;
+
+    _log(RETRO_LOG_DEBUG, "Loggin' in the air\n");
 }
 
 /* Library global initialization/deinitialization. */
@@ -69,10 +92,10 @@ RETRO_API void retro_get_system_info(retro_system_info *info)
 
 RETRO_API void retro_get_system_av_info(struct retro_system_av_info *info)
 {
-    info->geometry.max_height = 640;
-    info->geometry.max_width = 480;
-    info->geometry.base_height = 640;
-    info->geometry.base_width = 480;
+    info->geometry.max_width = 1920;
+    info->geometry.max_width = 1080;
+    info->geometry.base_height = 1920;
+    info->geometry.base_width = 1080;
     info->timing.fps = 60.0;
     info->timing.sample_rate = 44100.0;
 }
@@ -85,19 +108,6 @@ RETRO_API void retro_reset(void)
     // TODO: Random probability of failure
 }
 
-/* Runs the game for one video frame.
- * During retro_run(), input_poll callback must be called at least once.
- *
- * If a frame is not rendered for reasons where a game "dropped" a frame,
- * this still counts as a frame, and retro_run() should explicitly dupe
- * a frame if GET_CAN_DUPE returns true.
- * In this case, the video callback can take a NULL argument for data.
- */
-RETRO_API void retro_run()
-{
-    // TODO: Render the background
-}
-
 RETRO_API size_t retro_serialize_size(void)
 {
     return 0;
@@ -105,8 +115,8 @@ RETRO_API size_t retro_serialize_size(void)
 
 /* Serializes internal state. If failed, or size is lower than
  * retro_serialize_size(), it should return false, true otherwise. */
-RETRO_API bool retro_serialize(void *data, size_t size);
-RETRO_API bool retro_unserialize(const void *data, size_t size);
+RETRO_API bool retro_serialize(void *data, size_t size) { return false; }
+RETRO_API bool retro_unserialize(const void *data, size_t size) { return false; }
 
 RETRO_API void retro_cheat_reset() {}
 RETRO_API void retro_cheat_set(unsigned, bool, const char *) {}
@@ -141,4 +151,11 @@ RETRO_API void *retro_get_memory_data(unsigned id)
 RETRO_API size_t retro_get_memory_size(unsigned id)
 {
     return 0;
+}
+
+RETRO_API void retro_run()
+{
+    // TODO: Render the background
+    _input_poll();
+    //_video_refresh(nullptr, 480, 640, 480 * 4);
 }
