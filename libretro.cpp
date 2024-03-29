162112fa1e6bb435bf37c85431fad81db6ecefb9
Jesse Talavera <jesse@jesse.tg>
2024-03-29 08:45:12 -0400

    Embed icons for the buttons

--- libretro.cpp
+++ libretro.cpp
@@ -15,6 +15,12 @@
 #include <embedded/mctaylor_freezer.h>
 #include <embedded/mctaylor_bg.h>
 #include <embedded/mctaylor_lcd_font.h>
+#include <embedded/mcsoftserve_auto_button.h>
+#include <embedded/mcsoftserve_wash_button.h>
+#include <embedded/mcsoftserve_standby_button.h>
+#include <embedded/mctaylor_topping_button.h>
+#include <embedded/mcsoftserve_sel_button.h>
+#include <embedded/mcsoftserve_up_button.h>
 
 using std::array;
 
@@ -32,7 +38,15 @@ struct MachineState
 
 struct CoreState
 {
-    CoreState() noexcept
+    CoreState() noexcept :
+        _auto_button(embedded_mcsoftserve_auto_button, sizeof(embedded_mcsoftserve_auto_button), PNTR_SKYBLUE, false, false),
+        _wash_button(embedded_mcsoftserve_wash_button, sizeof(embedded_mcsoftserve_wash_button), PNTR_SKYBLUE, false, false),
+        _standby_button(embedded_mcsoftserve_standby_button, sizeof(embedded_mcsoftserve_standby_button), PNTR_SKYBLUE, false, false),
+        _topping_button_l(embedded_mctaylor_topping_button, sizeof(embedded_mctaylor_topping_button), PNTR_SKYBLUE, false, false),
+        _topping_button_r(embedded_mctaylor_topping_button, sizeof(embedded_mctaylor_topping_button), PNTR_SKYBLUE, true, false),
+        _sel_button(embedded_mcsoftserve_sel_button, sizeof(embedded_mcsoftserve_sel_button), PNTR_SKYBLUE, false, false),
+        _up_button(embedded_mcsoftserve_up_button, sizeof(embedded_mcsoftserve_up_button), PNTR_SKYBLUE, false, false),
+        _down_button(embedded_mcsoftserve_up_button, sizeof(embedded_mcsoftserve_up_button), PNTR_SKYBLUE, false, true)
     {
         audio_mixer_init(SAMPLE_RATE);
         _freezerSound = audio_mixer_load_wav(
@@ -71,6 +85,8 @@ struct CoreState
         _nk->style.window.spacing = {0, 0};
         _nk->style.window.group_padding = {0, 0};
         _nk->style.window.border = 0;
+
+
     }
 
     ~CoreState() noexcept
@@ -105,6 +121,45 @@ struct CoreState
 
     void Run();
 private:
+    struct button
+    {
+        pntr_image* normal_button = nullptr;
+        struct nk_image nk_normal_button {};
+        pntr_image* active_button = nullptr;
+        struct nk_image nk_active_button {};
+        nk_style_button style {};
+
+        button(const uint8_t* data, size_t length, pntr_color active_tint, bool flipx, bool flipy)
+        {
+            retro_assert(data != nullptr);
+            normal_button = pntr_load_image_from_memory(PNTR_IMAGE_TYPE_UNKNOWN, data, length);
+            pntr_image_flip(normal_button, flipx, flipy);
+
+            active_button = pntr_image_copy(normal_button);
+            retro_assert(active_button != nullptr);
+            pntr_image_color_tint(active_button, active_tint);
+
+            style.normal.type = NK_STYLE_ITEM_IMAGE;
+            style.normal.data.image = pntr_image_nk(normal_button);
+            style.active.type = NK_STYLE_ITEM_IMAGE;
+            style.active.data.image = pntr_image_nk(active_button);
+        }
+
+        button(const button&) = delete;
+        button& operator=(const button&) = delete;
+        button(button&&) = delete;
+        ~button() noexcept
+        {
+            pntr_unload_image(normal_button);
+            normal_button = nullptr;
+            nk_normal_button.handle.ptr = nullptr;
+
+            pntr_unload_image(active_button);
+            active_button = nullptr;
+            nk_active_button.handle.ptr = nullptr;
+        }
+    };
+
     audio_mixer_sound_t* _freezerSound = nullptr;
     audio_mixer_voice_t* _freezerVoice = nullptr;
     pntr_font* _font = nullptr;
@@ -112,6 +167,14 @@ private:
 
     struct nk_image _nk_steel_bg {};
     pntr_image* _steel_bg = nullptr;
+    button _auto_button;
+    button _wash_button;
+    button _standby_button;
+    button _topping_button_l;
+    button _topping_button_r;
+    button _sel_button;
+    button _up_button;
+    button _down_button;
     pntr_image* _framebuffer = nullptr;
 };
 
@@ -277,9 +340,40 @@ RETRO_API void retro_run()
     Core.Run();
 }
 
+// Thanks, Rob!
+float pntr_app_libretro_mouse_pointer_convert(float coord, float full, float margin)
+{
+    float max         = (float)0x7fff;
+    float screenCoord = (((coord + max) / (max * 2.0f) ) * full) - margin;
+
+    // Keep the mouse on the screen.
+    if (margin > 0.0f) {
+        float limit = full - (margin * 2.0f) - 1.0f;
+        screenCoord = (screenCoord < 0.0f)  ? 0.0f  : screenCoord;
+        screenCoord = (screenCoord > limit) ? limit : screenCoord;
+    }
+
+    return screenCoord + 0.5f;
+}
+
+
 void CoreState::Run()
 {
     _input_poll();
+
+    nk_input_begin(_nk);
+    {
+        int16_t mouseX = _input_state(0, RETRO_DEVICE_POINTER, 0, RETRO_DEVICE_ID_POINTER_X);
+        int16_t mouseY = _input_state(0, RETRO_DEVICE_POINTER, 0, RETRO_DEVICE_ID_POINTER_Y);
+        int16_t pressed = _input_state(0, RETRO_DEVICE_POINTER, 0, RETRO_DEVICE_ID_POINTER_PRESSED);
+
+        int screenX = pntr_app_libretro_mouse_pointer_convert(mouseX, SCREEN_WIDTH, 0);
+        int screenY = pntr_app_libretro_mouse_pointer_convert(mouseY, SCREEN_HEIGHT, 0);
+        nk_input_motion(_nk, screenX, screenY);
+        nk_input_button(_nk, NK_BUTTON_LEFT, screenX, screenY, pressed);
+    }
+    nk_input_end(_nk);
+
     array<float, SAMPLE_RATE * 2 / 60> buffer {};
     array<int16_t, SAMPLE_RATE * 2 / 60> outbuffer {};
 
@@ -287,6 +381,16 @@ void CoreState::Run()
     convert_float_to_s16(outbuffer.data(), buffer.data(), buffer.size());
 
     if (nk_begin(_nk, "", WINDOW_BOUNDS, WINDOW_FLAGS)) {
+        nk_layout_space_begin(_nk, NK_STATIC, 500, INT_MAX);
+        nk_layout_space_push(_nk, nk_rect(0,0,150,200));
+        if (nk_button_image_styled(_nk, nullptr, {}))
+        {
+
+        }
+        if (nk_button_color(_nk, nk_color{ 255, 0, 0, 255 })) {
+            _log(RETRO_LOG_DEBUG, "Button pressed\n");
+        }
+        nk_layout_space_end(_nk);
     }
     nk_end(_nk);
 
